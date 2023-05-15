8718c5c25c9528d1fdd8a1229d59b6512ef59c85
Riya <riya172444@gmail.com>
2023-05-15 14:02:15 +0530

    demo

---
+++ Semester4Project/src/main/java/com/example/Semester4Project/Semester4ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Semester4Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semester4ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Semester4ProjectApplication.class, args);
+	}
+
+}
---
+++ Semester4Project/src/test/java/com/example/Semester4Project/Semester4ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Semester4Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semester4ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
